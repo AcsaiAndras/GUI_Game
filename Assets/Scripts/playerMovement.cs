80593f137548c9501684792bbb2c1073e19578a3
AcsaiAndras <113457343+AcsaiAndras@users.noreply.github.com>
2023-05-25 12:22:04 +0200

    Fixing jump only on ground

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
+    BoxCollider2D bc;
+
     Animator anim;
     float dirX = 0f;
     SpriteRenderer sprite;
     [SerializeField] float moveSpeed = 7f;
     [SerializeField] float jumpForce = 16f;
+    [SerializeField] LayerMask jumpableGround;
 
     enum MovementState 
     {
@@ -22,6 +25,7 @@ public class playerMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
     }
@@ -33,7 +37,7 @@ public class playerMovement : MonoBehaviour
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x,jumpForce);
         }
@@ -75,5 +79,10 @@ public class playerMovement : MonoBehaviour
         }
 
         anim.SetInteger("state", (int)state);
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(bc.bounds.center,bc.bounds.size,0f,Vector2.down, .1f, jumpableGround);
     }
 }
